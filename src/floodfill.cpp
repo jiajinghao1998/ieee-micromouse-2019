cc0e2d0e2c5650382a158111db4b27ea9b1dbe1d
Jinghao Jia <jiajinghao1998@gmail.com>
2019-02-21 06:55:03 +0900

    Refactored cell getter and setters and added print maze function

--- src/floodfill.cpp
+++ src/floodfill.cpp
@@ -1,66 +1,99 @@
-#include <cstdlib>
 #include <iomanip>
 #include <cmath>
 #include "floodfill.hpp"
 
 namespace floodfill {
+  // Constructor
   cell::cell() : dist(0), flags(0) {}
 
-  bool cell::get_left() {
-    return (flags & (SETBITS << LEFT)) >> LEFT;
+  // Getter for wall flags, extract bit based on given directions
+  bool cell::get_wall_flag(direction dir) {
+    switch (dir) {
+      case direction::LEFT:
+        return (flags & (KEY << LEFT_BIT_IDX)) >> LEFT_BIT_IDX;
+      case direction::UP:
+        return (flags & (KEY << UP_BIT_IDX)) >> UP_BIT_IDX;
+      case direction::RIGHT:
+        return (flags & (KEY << RIGHT_BIT_IDX)) >> RIGHT_BIT_IDX;
+      case direction::DOWN:
+        return (flags & (KEY << DOWN_BIT_IDX)) >> DOWN_BIT_IDX;
+    }
+    return false;
   }
 
-  bool cell::get_up() {
-    return (flags & (SETBITS << UP)) >> UP;
-  }
-
-  bool cell::get_right() {
-    return (flags & (SETBITS << RIGHT)) >> RIGHT;
-  }
-
-  bool cell::get_down() {
-    return (flags & (SETBITS << DOWN)) >> DOWN;
-  }
-
+  // Getter for the visited flag of cell
   bool cell::get_visited() {
-    return flags & SETBITS;
+    return flags & KEY;
   }
 
-  void cell::set_left() {
-    flags |= (SETBITS << LEFT);
+  // Setter for wall flags, set bit based on given directions and target state
+  void cell::set_wall_flag(direction dir, bool on) {
+    switch (dir) {
+      case direction::LEFT:
+        flags |= on ? (KEY << LEFT_BIT_IDX) : ~(KEY << LEFT_BIT_IDX);
+      case direction::UP:
+        flags |= on ? (KEY << UP_BIT_IDX) : ~(KEY << UP_BIT_IDX);
+      case direction::RIGHT:
+        flags |= on ? (KEY << RIGHT_BIT_IDX) : ~(KEY << RIGHT_BIT_IDX);
+      case direction::DOWN:
+        flags |= on ? (KEY << DOWN_BIT_IDX) : ~(KEY << DOWN_BIT_IDX);
+    }
   }
 
-  void cell::set_up() {
-    flags |= (SETBITS << UP);
-  }
-
-  void cell::set_right() {
-    flags |= (SETBITS << RIGHT);
-  }
-
-  void cell::set_down() {
-    flags |= (SETBITS << DOWN);
-  }
-
+  // Setter for the visited flag of cell
   void cell::set_visited() {
-    flags |= SETBITS;
+    flags |= KEY;
   }
 
+  // Overloaded operator<< to print cell
   std::ostream& operator<<(std::ostream& os, cell& c) {
     os.fill('0');
     os << c.dist << " 0x" << std::hex << std::setw(4) << (int)c.flags;
     return os;
   }
 
+  // Definion of the external variables from header
   cell maze[SIDE_LENGTH][SIDE_LENGTH];
   std::stack<cell> st;
 
+  // Function to initialze maze and prepare for search
   void init() {
-    // TODO
-    std::cerr << "Init called" << std::endl;
+    // Use Manhattan distance from cneter as initial distance
+    const uint8_t mid = SIDE_LENGTH / 2;
+    for(uint8_t i = 0; i < SIDE_LENGTH; ++i) {
+      for(uint8_t j = 0; j < SIDE_LENGTH; ++j) {
+        maze[i][j] = cell();
+        maze[i][j].dist = m_dist(i, j, mid, mid);
+      }
+    }
+    print_maze(std::cout);
   }
 
+  // Manhattan distance calculation function
   uint8_t m_dist(uint8_t r1, uint8_t c1, uint8_t r2, uint8_t c2) {
     return abs(r1 - r2) + abs(c1 - c2);
+  }
+
+  // Function to visilize the maze and state of cells 
+  void print_maze(std::ostream& os) {
+    os.fill(' ');
+    for(uint8_t i = 0; i < SIDE_LENGTH; ++i) {
+      os << "+";
+      for(uint8_t j = 0; j < SIDE_LENGTH; ++j) {
+        os << (maze[i][j].get_wall_flag(direction::UP) ? "---" : "   ") << "+";
+      }
+      os << "\n" << (maze[i][0].get_wall_flag(direction::LEFT) ? "|" : " ");
+      for(uint8_t j = 0; j < SIDE_LENGTH; ++j) {
+        os << std::setw(3) << (int)maze[i][j].dist
+           << (maze[i][j].get_wall_flag(direction::RIGHT) ? "|" : " ");
+      }
+      os << "\n";
+    }
+    os << "+";
+    for(uint8_t j = 0; j < SIDE_LENGTH; ++j) {
+      os << (maze[SIDE_LENGTH - 1][j].get_wall_flag(direction::DOWN) ?
+             "---" : "   ") << "+";
+    }
+    os << std::endl;
   }
 }
--- src/testmain.cpp
+++ src/testmain.cpp
@@ -5,7 +5,8 @@ using namespace std;
 
 int main(void) {
   floodfill::maze[0][0] = floodfill::cell();
-  cout << (floodfill::maze[0][0].get_left() ? 1 : 0) << floodfill::maze[0][0] << endl;
+  cout << (floodfill::maze[0][0].get_wall_flag(floodfill::direction::LEFT)
+           ? 1 : 0) << floodfill::maze[0][0] << endl;
   floodfill::init();
   cout << (int)floodfill::m_dist(1, 2, 3, 4) << endl;
 }
